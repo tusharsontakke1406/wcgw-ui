e4f6fda619f7691caf816e1d48dd690f7901a8a8
tusharsontakke1406 <125453286+tusharsontakke1406@users.noreply.github.com>
2026-06-24 15:39:39 +0530

    [new branch]

---
+++ src/app/core/auth/role.guard.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { CanActivateFn } from '@angular/router';
+import { AuthService } from './auth.service';
+
+export const RoleGuard: CanActivateFn = (route, state) => {
+  const auth = inject(AuthService);
+  const requiredRoles = route.data?.['roles'] as string[];
+  if (requiredRoles && requiredRoles.some(role => auth.hasRole(role))) {
+    return true;
+  }
+  return false;
+};
