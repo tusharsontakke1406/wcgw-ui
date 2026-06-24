e2da2b783051e62e6401a489bb28dd48db0f4f1b
tusharsontakke1406 <125453286+tusharsontakke1406@users.noreply.github.com>
2026-06-24 15:39:51 +0530

    [new branch]

---
+++ src/app/features/dashboard/components/dashboard.component.spec.ts
@@ -0,0 +1,8 @@
+import { DashboardComponent } from './dashboard.component';
+
+describe('DashboardComponent', () => {
+  it('should create', () => {
+    const component = new DashboardComponent();
+    expect(component).toBeTruthy();
+  });
+});
