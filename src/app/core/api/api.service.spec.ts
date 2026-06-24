14ff84a22e77b6fe30461eb6e8ccb67a2f3a9c46
tusharsontakke1406 <125453286+tusharsontakke1406@users.noreply.github.com>
2026-06-24 15:39:52 +0530

    [new branch]

---
+++ src/app/core/api/api.service.spec.ts
@@ -0,0 +1,8 @@
+import { ApiService } from './api.service';
+
+describe('ApiService', () => {
+  it('should be created', () => {
+    const service = new ApiService({} as any);
+    expect(service).toBeTruthy();
+  });
+});
