3c89c18760874d93c85b94d3d7368f5cb589fca3
tusharsontakke1406 <125453286+tusharsontakke1406@users.noreply.github.com>
2026-06-24 15:39:50 +0530

    [new branch]

---
+++ src/app/core/api/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RiskKPI } from '../../features/dashboard/models/risk-kpi.model';
+import { EngagementFeedItem } from '../../features/dashboard/models/engagement-feed.model';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ApiService {
+  constructor(private http: HttpClient) {}
+
+  getRiskKPI(): Observable<RiskKPI> {
+    return this.http.get<RiskKPI>('/api/dashboard/kpi');
+  }
+
+  getEngagementFeed(): Observable<EngagementFeedItem[]> {
+    return this.http.get<EngagementFeedItem[]>('/api/dashboard/engagement-feed');
+  }
+}
