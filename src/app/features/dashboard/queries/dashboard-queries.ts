090311c0e9fd62181a1ea27b0c768cfddc7b1962
tusharsontakke1406 <125453286+tusharsontakke1406@users.noreply.github.com>
2026-06-24 15:39:45 +0530

    [new branch]

---
+++ src/app/features/dashboard/queries/dashboard-queries.ts
@@ -0,0 +1,23 @@
+import { Injectable, signal } from '@angular/core';
+import { TanStackQueryService } from '@tanstack/query-angular';
+import { ApiService } from '../../../core/api/api.service';
+import { RiskKPI } from '../models/risk-kpi.model';
+import { EngagementFeedItem } from '../models/engagement-feed.model';
+
+@Injectable({ providedIn: 'root' })
+export class DashboardQueries {
+  kpiQuery = this.queryService.createQuery<RiskKPI>({
+    queryKey: ['riskKPI'],
+    queryFn: () => this.api.getRiskKPI()
+  });
+
+  engagementFeedQuery = this.queryService.createQuery<EngagementFeedItem[]>({
+    queryKey: ['engagementFeed'],
+    queryFn: () => this.api.getEngagementFeed()
+  });
+
+  constructor(
+    private queryService: TanStackQueryService,
+    private api: ApiService
+  ) {}
+}
